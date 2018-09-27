4c0aac4a1344a5cc789fb0ec85b0b10e49f13215
2018JTM2250 <kartikgupta301@gmail.com>
2018-09-27 15:49:00 +0530

    Program ps2.py is executing succesfully

--- ps2.py
+++ ps2.py
@@ -1,33 +1,74 @@
+#
+#  @file ps2.py
+#
+#  @author Kartik Gupta
+#  @date 27 September 2018
+#
 
-
+# Creating groups for different characters
 group1 = list("abcdefghi")
 group2 = list("jklmnopqr")
 group3 = list("stuvwxyz_")
 
+# initializing list for groups
 gr1=[]
 gr2=[]
 gr3=[]
 gr1new=[]
 gr2new=[]
 gr3new=[]
+
+# initializing list for index
+index1=[]
+index2=[]
+index3=[]
+
+#Getting k1,k2 and k3 values
 k1,k2,k3 = [int(i) for i in input().split()]
 
+# getting input string
 word=input()
-list(word)
+wordlist=list(word)
 
-for i in word[:]:
-    if i in group1:
-        gr1.append(i)
-    elif i in group2:
-        gr2.append(i)
-    elif i in group3:
-        gr3.append(i)
+# Dividing the characters into different groups
+for i in range(len(word)):
+    if wordlist[i] in group1:
+        gr1.append(wordlist[i])
+        index1.append(i)
+    elif wordlist[i] in group2:
+        gr2.append(wordlist[i])
+        index2.append(i)
+    elif wordlist[i] in group3:
+        gr3.append(wordlist[i])
+        index3.append(i)
 
+# Rotating the groups
 gr1new = (gr1[-k1:] + gr1[:-k1])
 gr2new = (gr2[-k2:] + gr2[:-k2])
 gr3new = (gr3[-k3:] + gr3[:-k3])
 
-print(gr1)
-print(gr2)
-print(gr3)
+print(gr1new)
+print(gr2new)
+print(gr3new)
+
+
+#Getting decrypted msg
+p=q=r=0
+for i in range(0,len(word)):
+    if i in index1:
+        wordlist[i]=gr1new[p]
+        p+=1
+    elif i in index2:
+        wordlist[i]=gr2new[q]
+        q+=1
+    elif i in index3:
+        wordlist[i]=gr3new[r]
+        r+=1
+
+#displaying the decrypted msg
+print(wordlist)
+for i in wordlist[:]:
+    print (i, end ='')
+
+print("\n")
 
